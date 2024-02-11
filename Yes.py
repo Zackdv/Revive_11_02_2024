ea72134f44024113818c32b4c0190e09eb84e2f6
Zackdkv <deepak.deepak.kumar399@gmail.com>
2024-02-11 23:36:13 +0530

    Arithmetic

---
+++ Yes.py
@@ -0,0 +1,14 @@
+def Arithmetic(a,b):
+    add=a+b
+    print('The Sum:-',add)
+    sub=a-b
+    print('The Sub:-',sub)
+    multiply=a*b
+    print('The Multiply:-',multiply)
+    div=a/b
+    print('The divide:-',div)
+    return add,sub,multiply,div
+
+
+t=Arithmetic(74,85)
+
